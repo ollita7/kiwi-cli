a365de7ce37c92190543f7cecece26caa4c6d529
Guillermo Fernandez <guillermo.fernandez@cavepot.com>
2018-08-17 17:21:54 -0300

    no message

--- cli.js
+++ cli.js
@@ -6,9 +6,11 @@ var _ = require('lodash');
 program
   .version('0.1.0')
   .arguments('<name>')
+  .usage('kc [options] <name>')
   .option('-i, --init', 'creates initial sever', 'server.ts')
   .option('-c, --controller', 'creates a empty controller')
-  .option('-m, --middleware <type>', 'creates a empty controller')
+  .option('-m, --middleware <type>', 'creates a empty middleware')
+  .option('-b, --build', 'Compile the typescript code')
   .action(function (name, options) {
     var type = options.middleware;
     execute(name, type);
@@ -26,22 +28,34 @@ function create(name, content) {
 
 function execute(name, type) {
   if (program.init) {
-    const content = fs.readFileSync('templates/server.txt');
+    let content = fs.readFileSync(`${__dirname}/templates/server.txt`);
     create('server.ts', content);
+    content = fs.readFileSync(`${__dirname}/templates/tsconfig.json`);
+    create('tsconfig.json', content);
     return;
   }
   if (program.controller) {
-    let content = fs.readFileSync('templates/controller.txt');
+    let content = fs.readFileSync(`${__dirname}/templates/controller.txt`);
     content = _.replace(content, /{name}/g, name);
     create(`${name}.controller.ts`, content);
     return;
   }
   if (program.middleware) {
     const typeValue = type === 'after' || type === 'a' ? 'After' : 'Before'
-    let content = fs.readFileSync('templates/middleware.txt');
+    let content = fs.readFileSync(`${__dirname}/templates/middleware.txt`);
     content = _.replace(content, '{name}', name);
     content = _.replace(content, '{type}', typeValue);
     create(`${name}.middleware.${typeValue}.ts`, content);
     return;
+  }
+  if (program.build) {
+    var exec = require('child_process').exec;
+    exec(`${__dirname}/tsc`, function callback(error, stdout, stderr) {
+      if(error){
+        console.log(error);
+        return;
+      }
+      console.log('Compilation finished')
+    });
   }
 }
---
+++ readme.md
@@ -0,0 +1,22 @@
+
+# <img src="kiwi.png" width="120" alt="logo">
+Framework to help rest api development using typescript and node.
+
+# Table of Contents
+* [Installation](#installation)
+* [Commands](#commands)
+  
+## Installation
+1. Install module gobaly
+    `npm install kiwi-server-cli -g`
+
+## Commands
+1. Init
+    `kc init`
+2. Create controller
+    `kc -c <name of the controller>`
+3. Create Middlewares
+    `kc -m [ after | before ] <name of the middleware>`
+4. Build (NOT WORKING)
+    `kc -b`
+    
